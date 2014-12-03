3d8b9bbe1c2bea41420b88248324c19fa6204a58
Benjamin Sehl <sehl@whoisbenjam.in>
2014-12-02 22:37:24 -0500

    front-end

--- js/main.js
+++ js/main.js
@@ -1,38 +1,81 @@
-$('figure.effect-duke').css( 'height', function(halfwidth) {
-	halfwidth = $('body').width() / 2;
-	return halfwidth * 210 / 512;
-});
-
-
-if ($(window).width() <= 480){	
-		$('section.main').css( 'min-height', function(frontheight) {
-		frontheight = $(window).innerHeight() - ( 2 * $('figure.effect-duke').outerHeight());
-		return frontheight;
-	});
-	} else {
-	$('section.main').css( 'min-height', function(frontheight) {
-	frontheight = $(window).innerHeight() - $('figure.effect-duke').outerHeight();
-	return frontheight;
-	});
-}
-
-
-
-$(window).resize(function (){
+$(document).ready(function() {
+   
 	$('figure.effect-duke').css( 'height', function(halfwidth) {
 		halfwidth = $('body').width() / 2;
 		return halfwidth * 210 / 512;
 	});
 	
+	
 	if ($(window).width() <= 480){	
-		$('section.main').css( 'min-height', function(frontheight) {
-		frontheight = $(window).innerHeight() - ( 2 * $('figure.effect-duke').outerHeight());
-		return frontheight;
+			$('section.main').css( 'min-height', function(frontheight) {
+			frontheight = $(window).innerHeight() - ( 2 * $('figure.effect-duke').outerHeight());
+			return frontheight;
 		});
-	} else {
+		} else {
 		$('section.main').css( 'min-height', function(frontheight) {
 		frontheight = $(window).innerHeight() - $('figure.effect-duke').outerHeight();
 		return frontheight;
 		});
 	}
+	
+	
+	
+	$(window).resize(function (){
+		$('figure.effect-duke').css( 'height', function(halfwidth) {
+			halfwidth = $('body').width() / 2;
+			return halfwidth * 210 / 512;
+		});
+		
+		if ($(window).width() <= 480){	
+			$('section.main').css( 'min-height', function(frontheight) {
+			frontheight = $(window).innerHeight() - ( 2 * $('figure.effect-duke').outerHeight());
+			return frontheight;
+			});
+		} else {
+			$('section.main').css( 'min-height', function(frontheight) {
+			frontheight = $(window).innerHeight() - $('figure.effect-duke').outerHeight();
+			return frontheight;
+			});
+		}
+	});
+	
+	
+
+	
+	$('figcaption a').click(function(){
+										  
+	var hash = window.location.hash.substr(1);
+	var href = $('#nav li a').each(function(){
+		var href = $(this).attr('href');
+		if(hash===href.substr(0,href.length-5)){
+			var toLoad = hash+'.html #content';
+			$('#content').load(toLoad);
+		}											
+	});
+	var toLoad = $(this).attr('href')+' #content';
+	
+	
+		
+			function loadContent() {
+		$('#content').load(toLoad,'',showNewContent());
+	}
+	function showNewContent() {
+		$('#content').show('normal',hideLoader());
+	}
+	function hideLoader() {
+		$('#load').fadeOut('normal');
+	}
+
+
+
+		$('#content').hide('fast',loadContent);
+		$('#load').remove();
+		$('#wrapper').append('<span id="load">LOADING...</span>');
+		$('#load').fadeIn('normal');
+		window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+		
+		return false;	
+	});
+
+	
 });
