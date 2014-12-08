8d8d4fa4a931ca182f478e3cb358c3f4feb845bb
Benjamin Sehl <sehl@whoisbenjam.in>
2014-12-07 19:45:36 -0500

    lots!

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
    
 	$('figure.effect-duke').css( 'height', function(halfwidth) {
 		halfwidth = $('body').width() / 2;
-		return halfwidth * 210 / 512;
+		return halfwidth * 210 / 700; //512
 	});
 	
 	
@@ -23,7 +23,7 @@ $(document).ready(function() {
 	$(window).resize(function (){
 		$('figure.effect-duke').css( 'height', function(halfwidth) {
 			halfwidth = $('body').width() / 2;
-			return halfwidth * 210 / 512;
+			return halfwidth * 210 / 700;
 		});
 		
 		if ($(window).width() <= 480){	
@@ -39,43 +39,33 @@ $(document).ready(function() {
 		}
 	});
 	
-	
-
-	
+/*
 	$('figcaption a').click(function(){
-										  
-	var hash = window.location.hash.substr(1);
-	var href = $('#nav li a').each(function(){
-		var href = $(this).attr('href');
-		if(hash===href.substr(0,href.length-5)){
-			var toLoad = hash+'.html #content';
-			$('#content').load(toLoad);
-		}											
-	});
-	var toLoad = $(this).attr('href')+' #content';
-	
-	
-		
-			function loadContent() {
-		$('#content').load(toLoad,'',showNewContent());
-	}
-	function showNewContent() {
-		$('#content').show('normal',hideLoader());
-	}
-	function hideLoader() {
-		$('#load').fadeOut('normal');
-	}
-
-
-
+									  
+		var hash = window.location.hash.substr(1);
+		var href = $('#nav li a').each(function(){
+			href = $(this).attr('href');
+			if(hash===href.substr(0,href.length-5)){
+				var toLoad = hash+'.html #content';
+				$('#content').load(toLoad);
+			}											
+		});
+		var toLoad = $(this).attr('href')+' #content';
+		function loadContent() {
+			$('#content').load(toLoad,'',showNewContent());
+		}
+		function showNewContent() {
+			$('#content').show('normal',hideLoader());
+		}
+		function hideLoader() {
+			$('#load').fadeOut('normal');
+		}
 		$('#content').hide('fast',loadContent);
 		$('#load').remove();
 		$('#wrapper').append('<span id="load">LOADING...</span>');
 		$('#load').fadeIn('normal');
 		window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
-		
 		return false;	
 	});
-
-	
+*/
 });
