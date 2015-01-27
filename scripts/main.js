0627b0a5fc4d3ae78cc59dcd1a3f490eb3bd0bcf
Benjamin Sehl <sehl@whoisbenjam.in>
2015-01-28 00:13:55 +0200

    Who even knows what's changed …

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
+/* 
 
+var number, imageMargin;
+var firstImage =  $(".m-collage__image").first();
+var lastImage = $(".m-collage__image").last();
+
+$( ".m-collage__image" ).each(function( index ) {
+	
+	$(this).css('margin-top', function() {
+		
+		number = 1 + Math.floor(Math.random() * $(this).height() * 1.1);
+		imageMargin = number + (index * 100);
+		return imageMargin;
+		
+		});
+
+});
+
+$(".m-collage").css('height', function() {
+	$(firstImage).position();
+	$(lastImage).position();
+	return false;
+});
+
+*/
